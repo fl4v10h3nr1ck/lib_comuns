99035d2fe6522e88e414297af6366ba5f393a194
fl4v10h3nr1ck <flaviohenrique_2@yahoo.com.br>
2022-01-24 01:04:38 -0300

    versao final

---
+++ projeto/classes/teste/Main.java
@@ -0,0 +1,22 @@
+package teste;
+
+import comuns.Calculo;
+
+public class Main {
+
+	
+	
+	
+	
+	public static void main(String[] args){
+		
+		
+	
+	System.out.println(Calculo.compara("45565,000", "45565"));	
+	}
+	
+	
+	
+	
+	 
+}
